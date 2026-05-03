fcd809a6a8853ffc7b389fc12512978a4ff61b71
GOSHIKASNEHA <goshikasneha2005@gmail.com>
2026-05-04 00:30:53 +0530

    Final mini sprint script

--- api_script.py
+++ api_script.py
@@ -8,14 +8,13 @@ try:
     if response.status_code == 200:
         data = response.json()
 
-        # Save data to file
         with open("api_output.txt", "w") as file:
             file.write(str(data))
 
-        print("API data saved successfully!")
+        print("API call successful! Data saved.")
 
     else:
         print("Failed with status code:", response.status_code)
 
 except Exception as e:
-    print("Error:", e)
+    print("Error occurred:", e)
